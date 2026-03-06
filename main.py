0eace92c1b901b5665db6b0de925b935d189fac9
victorhasse <inicial-folgado0j@icloud.com>
2026-03-06 09:55:46 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import aiml
+import unicodedata
+import re
+
+
+# Efetua uma filtragem no texto de entrada
+def filter(text):
+    # Normaliza o texto entrado como parâmetro e remove os acentos
+    text = unicodedata.normalize("NFKD", text) \
+        .encode("ASCII", "ignore") \
+        .decode("utf-8")  # fmt: skip
+    # Remove pontuação e caracteres especiais
+    text = re.sub(r"[^\w\s]", "", text)
+    return text
+
+
+kb = sys.argv[1] if len(sys.argv) > 1 else "brain.xml" # Pega a base AIML entrada por parâmetro
+k = aiml.Kernel()  # Inicializa o motor de interpretação AIML
+k.learn(kb)  # Insere a base AIML no motor para aprendizado
+while True:  # Executa o laço indefinidamente
+    message = input("> ")  # Inicializa o prompt do chatbot
+    message = filter(message)  # Limpa a mensagem entrada pelo usuário
+    response = k.respond(message)  # Envia ao motor e retorna a resposta
+    print(response)  # Imprime a resposta
